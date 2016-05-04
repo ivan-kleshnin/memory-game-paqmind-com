5aa9413ad62153b016e771096ede6543eedeebc3
Ivan Kleshnin <ivan@paqmind.com>
2016-05-04 19:50:51 +0300

    Update types

--- src/types.js
+++ src/types.js
@@ -1,34 +1,56 @@
+let {always} = require("ramda")
 let T = require("tcomb")
 
-// Nat, Type -> Type
-let vect = (n, a, name) => {
-  let Vect = T.refinement(T.list(a), (x) => {
-    return x.length == n
-  }, name || `Vect(${n})`)
-  Vect.meta.size = n
-  return Vect
-}
+let Type = T.irreducible("Type", T.isType)
 
-// Nat, Nat, Type, String? -> Type
-let matrix = (m, n, a, name) => {
-  let Matrix = vect(m, vect(n, a), name || `Matrix(${m}, ${n})`)
-  Matrix.meta.size = [m, n]
-  return Matrix
-}
+let MaybeString = T.maybe(T.String, "MaybeString")
 
-let CellState = T.refinement(T.Number, (x) => {
+let NatNumber = T.refinement(T.Number, (x) => {
+  return !(x % 1)
+})
+
+// NatNumber, Type, MaybeString -> Type
+let vect = /*T
+  .func([NatNumber, Type, MaybeString], Type)
+  .of(*/(n, a, name) => {
+    let Vect = T.refinement(T.list(a), (x) => {
+      return x.length == n
+    }, name || `Vect(${n})`)
+    Vect.meta.size = n
+    return Vect
+  }/*)*/
+
+// NatNumber, NatNumber, Type, MaybeString -> Type
+let matrix = /*T
+  .func([NatNumber, NatNumber, Type, MaybeString], Type)
+  .of(*/(m, n, a, name) => {
+    let Matrix = vect(m, vect(n, a), name || `Matrix(${m}, ${n})`)
+    Matrix.meta.size = [m, n]
+    return Matrix
+  }/*)*/
+
+let CellPayload = T.refinement(T.String, always(true), "CellPayload")
+
+let CellState = T.refinement(NatNumber, (x) => {
   // 0: closed, 1: opened, 2: done
   return x == 0 || x == 1 || x == 2
 }, "CellState")
 
-let Cell = T.tuple([T.String, CellState])
+let Cell = T.tuple([CellPayload, CellState], "Cell")
 
-// Nat, Nat -> Type
-let board = (m, n) => {
-  return matrix(m, n, Cell)
-}
+// NatNumber, NatNumber, MaybeString -> Type
+let board = /*T
+  .func([NatNumber, NatNumber, MaybeString], Type)
+  .of(*/(m, n, name) => {
+    return matrix(m, n, Cell, name || `Board(${m}, ${n})`)
+  }/*)*/
 
+exports.Type = Type
+exports.MaybeString = MaybeString
+exports.NatNumber = NatNumber
+exports.vect = vect
 exports.matrix = matrix
+exports.CellPayload = CellPayload
 exports.CellState = CellState
 exports.Cell = Cell
 exports.board = board
